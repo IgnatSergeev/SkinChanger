91c0ad3395e57cf33e5bd8100ac0f8b734a2adc4
IgnatSergeev/laptop <ignatii.f.sergeev@gmail.com>
2023-04-01 22:46:06 +0300

    Init commit

---
+++ src/SkinChanger.c
@@ -0,0 +1,18 @@
+#include <windows.h>
+
+HANDLE hProcess = NULL;
+
+DWORD dwClientState = 0;
+DWORD dwLocalPlayer = 0;
+DWORD dwEntityList = 0;
+DWORD m_hViewModel = 0;
+DWORD m_iViewModelIndex = 0;
+DWORD m_flFallbackWear = 0;
+DWORD m_nFallbackPaintKit = 0;
+DWORD m_iItemIDHigh = 0;
+DWORD m_iEntityQuality = 0;
+DWORD m_iItemDefinitionIndex = 0;
+DWORD m_hActiveWeapon = 0;
+DWORD m_hMyWeapons = 0;
+DWORD m_nModelIndex = 0;
+DWORD m_dwModelPrecache = 0;
